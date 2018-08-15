6de2e03370f2216430bf361f1e6c78e1ae2cfd59
Lauren Hallden <lauren@stitchdata.com>
2018-08-15 18:11:29 -0400

    starting to work on logic

--- js/bodyparts.js
+++ js/bodyparts.js
@@ -2,192 +2,55 @@
 // Parts can either have a count of 1 (like your spine), 2 (like your legs), 10 (like a set of toes),
 // or 20 for groups of things too big to be counted (like pores) and should always be talked about as a group
 
-var bodyparts = {
- ankle: [{
-        plural: "ankles",
-        count: 2
-    }],
- arm: [{
-        plural: "arms",
-        count: 2
-    }],
- back: [{
-        plural: "ankles",
-        count: 1
-    }],
- "belly button": [{
-        count: 1
-    }],
- bicep: [{
-        plural: "biceps",
-        count: 2
-    }],
- boob: [{
-        plural: "boobs",
-        count: 2
-    }],
- bottom: [{
-        count: 1
-    }],
- breast: [{
-        plural: "breasts",
-        count: 2
-    }],
- butt: [{
-        count: 1
-    }],
- calf: [{
-        plural: "calves",
-        count: 2
-    }],
- cheek: [{
-        plural: "cheeks",
-        count: 2
-    }],
- chest: [{
-        count: 1
-    }],
- '"down there"': [{
-        count: 1
-    }],
- earlobe: [{
-        plural: "earlobes",
-        count: 2
-    }],
- ear: [{
-        plural: "ears",
-        count: 2
-    }],
- elbow: [{
-        plural: "elbows",
-        count: 2
-    }],
- eyebrow: [{
-        plural: "eyebrows",
-        count: 2
-    }],
- eyelash: [{
-        plural: "eyelashes",
-        count: 10
-    }],
- eyelids: [{
-        plural: "eyelids",
-        count: 2
-    }],
- eye: [{
-        plural: "eyes",
-        count: 2
-    }],
- face: [{
-        count: 1
-    }],
- foot: [{
-        plural: "feet",
-        count: 2
-    }],
- forearm: [{
-        plural: "forearms",
-        count: 2
-    }],
- freckle: [{
-        plural: "freckles",
-        count: 20
-    }],
- hairline: [{
-        count: 1
-    }],
- hair: [{
-        count: 1
-    }],
- heel: [{
-        plural: "heels",
-        count: 2
-    }],
- hipbone: [{
-        plural: "hipbones",
-        count: 2
-    }],
- hip: [{
-        plural: "hips",
-        count: 2
-    }],
- knee: [{
-        plural: "knees",
-        count: 2
-    }],
- leg: [{
-        plural: "legs",
-        count: 2
-    }],
- lip: [{
-        plural: "lips",
-        count: 2,
-        set: "vertical"
-    }],
- "lower back": [{
-        count: 1
-    }],
- "nail bed": [{
-        plural: "nail beds",
-        count: 10
-    }],
- nail: [{
-        plural: "nails",
-        count: 10
-    }],
- neck: [{
-        count: 1
-    }],
- nipple: [{
-        plural: "nipples",
-        count: 2
-    }],
- pore: [{
-        plural: "pores",
-        count: 20
-    }],
- rib: [{
-        plural: "ribs",
-        count: 2
-    }],
- shoulder: [{
-        plural: "shoulders",
-        count: 2
-    }],
- spine: [{
-        count: 1
-    }],
- "split end": [{
-        plural: "split ends",
-        count: 20
-    }],
- stomach: [{
-        count: 1
-    }],
- "tear duct": [{
-        plural: "tear ducts",
-        count: 2
-    }],
- thigh: [{
-        plural: "thighs",
-        count: 2
-    }],
- tooth: [{
-        plural: "teeth",
-        count: 20
-    }],
- toenail: [{
-        plural: "toenails",
-        count: 10
-    }],
- toe: [{
-        plural: "toes",
-        count: 10
-    }],
- tummy: [{
-        count: 1
-    }],
- vagina: [{
-        count: 1
-    }]
-};
+var bodyparts = [
+ {singular: "ankle", plural: "ankles", count: 2},
+ {singular: "arm", plural: "arms", count: 2},
+ {singular: "back", plural: "ankles", count: 1},
+ {singular: "belly button", count: 1},
+ {singular: "bicep", plural: "biceps", count: 2},
+ {singular: "boob", plural: "boobs", count: 2},
+ {singular: "bottom", count: 1},
+ {singular: "breast", plural: "breasts", count: 2},
+ {singular: "butt", count: 1},
+ {singular: "calf", plural: "calves", count: 2},
+ {singular: "cheek", plural: "cheeks", count: 2},
+ {singular: "chest", count: 1},
+ {singular: "down there", count: 1},
+ {singular: "earlobe", plural: "earlobes", count: 2},
+ {singular: "ear", plural: "ears", count: 2},
+ {singular: "elbow", plural: "elbows", count: 2},
+ {singular: "eyeborw", plural: "eyebrows", count: 2},
+ {singular: "eyelash", plural: "eyelashes", count: 10},
+ {singular: "eyelid", plural: "eyelids", count: 2},
+ {singular: "eye", plural: "eyes", count: 2},
+ {singular: "face", count: 1},
+ {singular: "foot", plural: "feet",  count: 2},
+ {singular: "forearm", plural: "forearms", count: 2},
+ {singular: "freckle", plural: "freckles", count: 20},
+ {singular: "hairline", count: 1},
+ {singular: "hair", count: 1},
+ {singular: "heel", plural: "heels", count: 2},
+ {singular: "hipbone", plural: "hipbones", count: 2},
+ {singular: "hip", plural: "hips", count: 2},
+ {singular: "knee", plural: "knees", count: 2},
+ {singular: "leg", plural: "legs", count: 2},
+ {singular: "lip", plural: "lips", count: 2, set: "vertical"},
+ {singular: "lower back", count: 1},
+ {singular: "nail bed", plural: "nail beds", count: 10},
+ {singular: "nail", plural: "nails", count: 10},
+ {singular: "neck", count: 1},
+ {singular: "nipple", plural: "nipples", count: 2},
+ {singular: "pore", plural: "pores", count: 20},
+ {singular: "rib", plural: "ribs", count: 2},
+ {singular: "shoulder", plural: "shoulders", count: 2},
+ {singular: "spine", count: 1},
+ {singular: "split end", plural: "split ends", count: 20},
+ {singular: "stomach", count: 1},
+ {singular: "tear duct", plural: "tear ducts", count: 2},
+ {singular: "thigh", plural: "thighs", count: 2},
+ {singular: "tooth", plural: "teeth", count: 20},
+ {singular: "toenail", plural: "toenails", count: 10},
+ {singular: "toe", plural: "toes", count: 10},
+ {singular: "tummy", count: 1},
+ {singular: "vagina", count: 1}
+];
--- js/problems.js
+++ js/problems.js
@@ -2,12 +2,12 @@
 // so we can solve them later
 
 var problems = {
- shape: [square, triangular, oblong, pointy, sharp, long, knobby],
- position: ["far apart", "close together", high, low],
- big: [visibile, prominent, fat, hefty, bulky, big, strong],
- small: [shrimpy, puny, small, thin, weak, timid, shy, tiny, meek, bashful],
- age: [old, "worn down", grungy, "not bright anymore", ashy, dull, dingy, sad, tired],
- texture: [wrinkled, dented, ashy, rough, scaly, dry, hairy, fuzzy, fluffy],
- attitude: [angry, unruly, rowdy, unmanageable, "out of control", grumpy, noisy],
- smell: [stinky, smelly, pungent]
+ shape: ["square", "triangular", "oblong", "pointy", "sharp", "long", "knobby"],
+ position: ["far apart", "close together", "high", "low"],
+ big: ["visibile", "prominent", "fat", "hefty", "bulky", "big", "strong"],
+ small: ["shrimpy", "puny", "small", "thin", "weak", "timid", "shy", "tiny", "meek", "bashful"],
+ age: ["old", "worn down", "grungy", "not bright anymore", "ashy", "dull", "dingy", "sad", "tired"],
+ texture: ["wrinkled", "dented", "ashy", "rough", "scaly", "dry", "hairy", "fuzzy", "fluffy"],
+ attitude: ["angry", "unruly", "rowdy", "unmanageable", "out of control", "grumpy", "noisy"],
+ smell: ["stinky", "smelly", "pungent"]
 };
