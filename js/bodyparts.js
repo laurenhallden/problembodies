b1c7114886e70a51915a96aa20a10e47c0118e8f
laurenhallden <lhallden@rjmetrics.com>
2018-08-15 07:47:08 -0400

    adding lists of parts and problems

---
+++ js/bodyparts.js
@@ -0,0 +1,193 @@
+// Make an object with all problem body parts
+// Parts can either have a count of 1 (like your spine), 2 (like your legs), 10 (like a set of toes),
+// or 20 for groups of things too big to be counted (like pores) and should always be talked about as a group
+
+var bodyparts = {
+ ankle: [
+        plural : "ankles",
+        count: 2
+    ],
+ arm: [
+        plural : "arms",
+        count : 2
+    ],
+ back: [
+        plural : "ankles",
+        count : 1
+    ],
+ "belly button": [
+        count : 1
+    ],
+ bicep: [
+        plural : "biceps",
+        count : 2
+    ],
+ boob: [
+        plural : "boobs",
+        count : 2
+    ],
+ bottom: [
+        count : 1
+    ],
+ breast: [
+        plural : "breasts",
+        count : 2
+    ],
+ butt: [
+        count : 1
+    ],
+ calf: [
+        plural : "calves",
+        count : 2
+    ],
+ cheek: [
+        plural : "cheeks",
+        count : 2
+    ],
+ chest: [
+        count : 1
+    ],
+ '"down there"': [
+        count : 1
+    ],
+ earlobe: [
+        plural : "earlobes",
+        count : 2
+    ],
+ ear: [
+        plural : "ears",
+        count : 2
+    ],
+ elbow: [
+        plural : "elbows",
+        count : 2
+    ],
+ eyebrow: [
+        plural : "eyebrows",
+        count : 2
+    ],
+ eyelash: [
+        plural : "eyelashes",
+        count : 10
+    ],
+ eyelids: [
+        plural : "eyelids",
+        count : 2
+    ],
+ eye: [
+        plural : "eyes",
+        count : 2
+    ],
+ face: [
+        count : 1
+    ],
+ foot: [
+        plural : "feet",
+        count : 2
+    ],
+ forearm: [
+        plural : "forearms",
+        count : 2
+    ],
+ freckle: [
+        plural : "freckles",
+        count : 20
+    ],
+ hairline: [
+        count : 1
+    ],
+ hair: [
+        count : 1
+    ],
+ heel: [
+        plural : "heels",
+        count : 2
+    ],
+ hipbone: [
+        plural : "hipbones",
+        count : 2
+    ],
+ hip: [
+        plural : "hips",
+        count : 2
+    ],
+ knee: [
+        plural : "knees",
+        count : 2
+    ],
+ leg: [
+        plural : "legs",
+        count : 2
+    ],
+ lip: [
+        plural : "lips",
+        count : 2,
+        set : "vertical"
+    ],
+ "lower back": [
+        count : 1
+    ],
+ "nail bed": [
+        plural : "nail beds",
+        count : 10
+    ],
+ nail: [
+        plural : "nails",
+        count : 10
+    ],
+ neck: [
+        count : 1
+    ],
+ nipple: [
+        plural : "nipples",
+        count : 2
+    ],
+ pore: [
+        plural : "pores",
+        count : 20
+    ],
+ rib: [
+        plural : "ribs",
+        count : 2
+    ],
+ shoulder: [
+        plural : "shoulders",
+        count : 2
+    ],
+ spine: [
+        count : 1
+    ],
+ "split end": [
+        plural : "split ends",
+        count : 20
+    ],
+ stomach: [
+        count : 1
+    ],
+ "tear duct": [
+        plural : "tear ducts",
+        count : 2
+    ],
+ thigh: [
+        plural : "thighs",
+        count : 2
+    ],
+ tooth: [
+        plural : "teeth",
+        count : 20
+    ],
+ toenail: [
+        plural : "toenails",
+        count : 10
+    ],
+ toe: [
+        plural : "toes",
+        count : 10
+    ],
+ tummy: [
+        count : 1
+    ],
+ vagina: [
+        count : 1
+    ]
+};
---
+++ js/problems.js
@@ -0,0 +1,13 @@
+// Make an object containing all the categories of ways things can be problems,
+// so we can solve them later
+
+var problems = {
+ shape: [square, triangular, oblong, pointy, sharp, long, knobby, high, low],
+ position: ["far apart", "close together"],
+ big: [visibile, prominent, fat, hefty, bulky, big, strong],
+ small: [shrimpy, puny, small, thin, weak, timid, shy, tiny, meek, bashful],
+ age: [old, "worn down", grungy, "not bright anymore", ashy, dull, dingy, sad, tired],
+ texture: [wrinkled, dented, ashy, rough, scaly, dry, hairy, fuzzy, fluffy],
+ attitude: [angry, unruly, rowdy, unmanageable, "out of control", grumpy, noisy],
+ smell: [stinky, smelly, pungent]
+};
